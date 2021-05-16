a1e6b7b169a386dc8769356fde6f5b01aee28f48
Alexandr Z <gopokemon1739@gmail.com>
2021-05-16 18:31:52 +0300

    123

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!12323213213213213213213'
 
+#123123132131132
 
 if __name__ == '__main__':
     app.run()
