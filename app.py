e3f82e2560b67b1daa0ef90b231fb2f29f2a242d
Sedoi <sedoi9841@gmail.com>
2021-05-16 23:12:51 +0700

    добавил буквы

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello World!12323213213213213213213'
     return 'Hello World!12323213213213213213213'
 
-#1231231321311321212121
+#1231231321311321212121blabla
 
 if __name__ == '__main__':
     app.run()
